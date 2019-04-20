c8ba4fd48869098291c03fcf312abc4fab239721
kenyangnov <2275169040@qq.com>
2019-04-20 23:03:08 +0800

    dp和递归进一步理解

--- hiho/hiho250_ParenthesesSequence.cpp
+++ hiho/hiho250_ParenthesesSequence.cpp
@@ -9,7 +9,7 @@ struct parenthese {
 	int lp;
 	int rp;
 };
-string ToSr(const string &s) {
+string ToSr(const string &s, int &edge) {
 	string str = s;
 	int len = s.length();
 	//找SL、SR分界
@@ -34,7 +34,8 @@ string ToSr(const string &s) {
 	}
 	int sum_lp = cur[len - 1].lp;
 	int sum_rp = cur[len - 1].rp;
-	int edge = 0;
+	
+	edge = 0;
 	bool flag = false; //标志是否需要翻转
 	for (int i = 0; i < len; i++) {
 		edge = i;
@@ -43,12 +44,12 @@ string ToSr(const string &s) {
 			break;
 		}
 	}
-/*
+
 	for (int i = 0; i < len; i++) {
 		cout <<i<<"---"<< cur[i].lp << "---" << cur[i].rp << endl;
 	}
 	cout << edge << endl;
-*/
+
 	delete[] cur;
 	if (!flag) { //不含SL，无需翻转，直接返回str
 		return str;
@@ -63,10 +64,12 @@ string ToSr(const string &s) {
 			str[i] = '(';
 		}
 	}
+	cout << "互换：" << str << endl;
 	i = 0, j = edge;
 	while (i < j) {
 		swap(str[i++], str[j--]);
 	}
+	cout << "翻转：" << str << endl;
 	// 返回处理后的字符串
 	return str;
 }
@@ -85,24 +88,6 @@ int GetMinP(const string &s) {
 	return cnt;
 }
 
-//dp:
-int f(int ** &dp, int i, int j, int *limit) {
-	if (i == 0) {
-		return 1;
-	}
-	int sum = 0;
-	if (j > limit[i]) {
-		sum = 0;
-	}
-	else {
-		for (int k = 0; k <= limit[i - 1]; k++) {
-			sum += f(dp, i - 1, k, limit);
-		}
-	}
-	dp[i][j] = sum;
-	return dp[i][j];
-}
-
 int GetT(const string &s) {
 	//打表
 	int cnt = 1; //第一个左括号计数（第一个一定是左括号）
@@ -119,11 +104,11 @@ int GetT(const string &s) {
 		}
 	}
 	limit[limit_num] = cnt; //最后一个位置可以插入右括号
-	/*
+
 	for (int i = 0; i < limit_num + 1; i++) {
 		cout << i << "--" << limit[i] << endl;
 	}
-	*/
+
 
 	//动态规划
 	//f[i][j]表示在前i个位置一共插入了j个右括号有多少种方法
@@ -134,15 +119,39 @@ int GetT(const string &s) {
 	for (int i = 0; i < limit_num + 1; i++) {
 		dp[i] = new int[cnt + 1];
 	}
-	int result = f(dp, limit_num, cnt, limit);
-/*
+	for (int i = 0; i < limit_num + 1; i++) {
+		if (i == 0) {
+			dp[i][0] = 1;
+			dp[i][1] = 1;
+			for (int j = 2; j < cnt + 1; j++) {
+				dp[i][j] = 0;
+			}
+		}
+		else {
+			for (int j = 0; j < cnt + 1; j++) {
+				if (j <= limit[i]) {
+					int sum = 0;
+					int min = j < limit[i - 1] ? j : limit[i - 1];
+					for (int k = 0; k <= min; k++) {
+						sum += dp[i - 1][k];
+					}
+					dp[i][j] = sum;
+				}
+				else {
+					dp[i][j] = 0;
+				}
+			}
+		}
+	}
+	int result = dp[limit_num][cnt];
+
 	for (int i = 0; i < limit_num + 1; i++) {
 		for (int j = 0; j < cnt + 1; j++) {
 			cout << dp[i][j] << "--";
 		}
 		cout << "*" << endl;
 	}
-*/
+
 	for (int i = 0; i < limit_num; i++) {
 		delete[] dp[i];
 	}
@@ -154,15 +163,26 @@ int main()
 {
 	string s;
 	cin >> s;
-	string str = ToSr(s);	//将字符串转换为SR形
-	//cout << str << endl;
+	int edge = 0;
+	string str = ToSr(s, edge);	//将字符串转换为SR形
+	cout << str << endl;
 	int min = GetMinP(str); //计算添加括号的最小值
-	cout << min << " ";
-	int t = GetT(str); //计算插入括号的不同组合数
+	cout << min << endl;
+
+	//计算插入括号的不同组合数
+	unsigned int t = 0;
+	if (edge == 0 || edge == str.length()-1) {
+		t = GetT(str); 
+	}
+	else{
+		int t1 = GetT(str.substr(0, edge));
+		cout << str.substr(0, edge+1) << ":" << t1 << endl;
+		int t2 = GetT(str.substr(edge+1));
+		cout << str.substr(edge + 1) << ":" << t2 << endl;
+		t = t1 * t2;
+	}
+	
 	cout << t << endl;
-	//system("pause");
+	system("pause");
+	return 0;
 }
-//dp：
-//令f[i][j]表示在前i个位置一共插入了j个右括号有多少种方法。如果j > limit[i]则f[i][j] = 0；
-//否则 f[i][j] = f[i - 1][0] + f[i - 1][1] + ... f[i - 1][limit[i - 1]]
-//如何转换成代码？
