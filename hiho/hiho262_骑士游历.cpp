22d1a0fe473bad96711a6a80b3d7f321f6fb28b8
kenyangnov <2275169040@qq.com>
2019-07-14 15:09:50 +0800

    快速幂

--- hiho/hiho262_骑士游历.cpp
+++ hiho/hiho262_骑士游历.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
 long long grid[64][64];
@@ -7,7 +7,7 @@ long long res[64][64];
 long long m = 1000000007;
 long long n, r, c;
 
-//初始化邻接矩阵
+//初始化变换矩阵grid
 //用于表示两点之间是否可达(8*8棋盘->64*64两点关系)
 //方向：int dir[8][2] = {{-2,-1},{-2,1},{-1,-2},{-1,2},{2,-1},{1,-2},{1,2},{2,1}};
 void initGrid()
@@ -59,7 +59,7 @@ void mul1() // res * grid
 		for (int j = 0; j < 64; j++) {
 			for (int k = 0; k < 64; k++) {
 				temp[i][j] += res[i][k] * grid[k][j];
-				temp[i][j] %= m;
+				temp[i][j] %= m;	//取模防止溢出
 			}
 		}
 	}
@@ -70,7 +70,7 @@ void mul1() // res * grid
 	}
 }
 
-void mul2() // grid * grid
+void mul2() // grid * grid（即更新base）
 {
 	long long temp[64][64];
 	memset(temp, 0, sizeof(temp));
@@ -91,6 +91,7 @@ void mul2() // grid * grid
 
 void Factor(long long n)
 {
+	//初始化结果矩阵
 	for (int i = 0; i < 64; i++) {
 		for (int j = 0; j < 64; j++) {
 			if (i == j) {
@@ -102,6 +103,7 @@ void Factor(long long n)
 			}
 		}
 	}
+	//矩阵快速幂
 	while (n) {
 		if (n & 1) {
 			mul1();
@@ -119,6 +121,7 @@ int main(int argc, const char* argv[]) {
 	Factor(n);
 	long long ans = 0;
 	long long index = (r - 1) * 8 + c - 1;
+	//通过计算index列的和，统计走法的总数
 	for (int i = 0; i < 64; i++) {
 		ans += res[index][i];
 		ans %= m;
@@ -128,3 +131,19 @@ int main(int argc, const char* argv[]) {
 }
 
 
+//快速幂(二进制的作用是标记，决定基数要不要乘进ans)
+int poww(int a, int b) {
+	int ans = 1, base = a; 
+	while (b != 0) {
+		if (b & 1) {
+			ans *= base;
+		}
+		base *= base;
+		b >>= 1;
+	}
+	return base;
+}
+
+//矩阵快速幂，用于求解递推式
+//可以在O(logn)复杂度下计算出斐波那契数列第n项
+//关系矩阵为{1, 1; 1, 0}
