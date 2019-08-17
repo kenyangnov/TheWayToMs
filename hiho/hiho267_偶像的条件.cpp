a96efbe025baa03766e333531b32e1ad9dafb2b3
kenyangnov <2275169040@qq.com>
2019-08-17 22:32:59 +0800

    找相邻最近的策略：排序后找略大和略小的

--- hiho/hiho267_偶像的条件.cpp
+++ hiho/hiho267_偶像的条件.cpp
@@ -9,25 +9,25 @@ int a[maxn], b[maxn], c[maxn];
 
 int main() {
 	cin >> n >> m >> l;
-	for (int i = 1; i <= n; i++) cin >> a[i];
-	for (int i = 1; i <= m; i++) cin >> b[i];
-	for (int i = 1; i <= l; i++) cin >> c[i];
+	for (int i = 0; i <= n - 1; i++) cin >> a[i];
+	for (int i = 0; i <= m - 1; i++) cin >> b[i];
+	for (int i = 0; i <= l - 1; i++) cin >> c[i];
 	//首先排序
-	sort(b + 1, b + 1 + m);
-	sort(c + 1, c + 1 + l);
+	sort(b, b + m);
+	sort(c, c + l);
 	int ans = INF;
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		// 身高大于等于A[i]的最矮的人为：B[j+1]和C[k+1]，身高小于等于Ai的最高的人：B[j]和C[k]
+		// 在B、C中找到身高大于等于A[i]的最矮的人，身高小于Ai的最高的人
 		// 注:lower_bound函数返回从first开始的第一个大于或等于val的元素的地址（数组必须是排好序的数组）
-		int j = lower_bound(b + 1, b + 1 + m, a[i]) - (b + 1); //相对于(b+1)的偏移量,即身高小于等于Ai的最高的人
-		int k = lower_bound(c + 1, c + 1 + l, a[i]) - (c + 1);
-		if(j>0 && k>0) ans = min(ans, abs(a[i] - b[j]) + abs(a[i] - c[k]) + abs(b[j] - c[k]));
-		if (k > 0)  ans = min(ans, abs(a[i] - b[j + 1]) + abs(a[i] - c[k]) + abs(b[j + 1] - c[k]));
-		if (j > 0)  ans = min(ans, abs(a[i] - b[j]) + abs(a[i] - c[k + 1]) + abs(b[j] - c[k + 1]));
-		ans = min(ans, abs(a[i] - b[j + 1]) + abs(a[i] - c[k + 1]) + abs(b[j + 1] - c[k + 1]));
+		int j = lower_bound(b, b + m, a[i]) - b; //大于或等于A[i]的最矮的人(若存在)/小于A[i]最高的人
+		int k = lower_bound(c, c + l, a[i]) - c;
+
+		ans = min(ans, abs(a[i] - b[j]) + abs(a[i] - c[k]) + abs(b[j] - c[k]));	//B、C中都取大于A最低的
+		if ((j - 1) >= 0)ans = min(ans, abs(a[i] - b[j - 1]) + abs(a[i] - c[k]) + abs(b[j - 1] - c[k]));	//B取小于A最高的，C取大于A最低的
+		if ((k - 1) >= 0)ans = min(ans, abs(a[i] - b[j]) + abs(a[i] - c[k - 1]) + abs(b[j] - c[k - 1]));	//B取大于A最低的，C取小于A最高的
+		if ((j - 1) >= 0 && (k - 1) >= 0)ans = min(ans, abs(a[i] - b[j - 1]) + abs(a[i] - c[k - 1]) + abs(b[j - 1] - c[k - 1]));	//B、C都取大于A最高的
 	}
 	cout << ans << endl;
-	system("pause");
 	return 0;
 }
