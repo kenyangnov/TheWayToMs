e5b8e1e9085a64815ff9f357b52d2768daf34348
kynov <2275169040@qq.com>
2020-06-05 19:32:05 +0800

    TLE

--- hiho/hiho308_影院座位.cpp
+++ hiho/hiho308_影院座位.cpp
@@ -1,95 +1,53 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
-int n, m;
-vector<bool> seats;
-vector<bool> expand_seats;
-vector<int> avail_seats;
-vector<int> next_seat;
-
 int main() {
+    // Init
+    int n, m;
     cin >> n >> m;
+    vector<int> seats(3 * n), nextSeat(3 * n);
     int seats_cnt = 0;
-    for (int i = 0; i < n; i++) {
-        bool s;
-        cin >> s;
-        seats.push_back(s);
-        if (s == 0) {
-            seats_cnt++;
+    for (int i = n; i < 2 * n; i++) {
+        cin >> seats[i];
+        seats[i - n] = seats[i];
+        seats[i + n] = seats[i];
+        // if (seats[i] == 0) {
+        //     seats_cnt++;
+        // }
+        seats_cnt += (seats[i] == 0);
+    }
+    int last = 3 * n;
+    for (int i = 3 * n - 1; i >= 0; i--) {
+        nextSeat[i] = last;
+        if (seats[i] == 0) {
+            last = i;
         }
     }
+    // Processing
     if (seats_cnt < m) {
-        return -1;
-    }
-
-    // expand_seats.insert(expand_seats.end(), seats.begin(), seats.end());
-    // expand_seats.insert(expand_seats.end(), seats.begin(), seats.end());
-    // expand_seats.insert(expand_seats.end(), seats.begin(), seats.end());
-
-    int pre = 0;
-    for (int i = 0; i < expand_seats.size(); i++) {
-        if (expand_seats[i] == 0) {
-            for (int j = pre; j < i; j++) {
-                next_seat.push_back(i);
+        cout << -1 << endl;
+    } else {
+        long long ans = LLONG_MAX;
+        for(int i=n; i<2*n; i++){
+            int l=i, r=i, s = m-(1-seats[i]);
+            long long sum = 0;
+            while (s > 0) {
+                if (s > 0) {
+                    l--;
+                    if (seats[l] == 0) s--, sum = sum + (i - l);
+                }
+                if (s > 0) {
+                    r++;
+                    if (seats[r] == 0) s--, sum = sum + (r - i);
+                }
             }
-            pre = i;
+            ans = min(ans, sum);
         }
+        cout << ans << endl;
     }
-
-    int ans = n * m;
-    int start = n;
-    int end = start + n;
-
-    int l = start, r = start, s = m - (1 - expand_seats[start]), sum = 0,
-        lc = 1 - expand_seats[start], rc = 0;
-    while (s > 0) {
-        if (s > 0) {
-            l = l - 1;
-            if (expand_seats[l] == 0) {
-                s = s - 1;
-                sum = sum + (start - l);
-                lc++;
-            }
-        }
-        if (s > 0) {
-            r = r + 1;
-            if (expand_seats[r] == 0) {
-                s = s - 1;
-                sum = sum + (r - start);
-                rc++;
-            }
-        }
-    }
-
-    while (expand_seats[l] != 0) l++;
-    while (expand_seats[r] != 0) r--;
-
-    ans = min(ans, sum);
-
-    for (int i = n + 1; i < 2 * n; i++) {
-        sum = sum + lc - rc;
-        if (expand_seats[i] == 0) {
-            lc = lc + 1;
-            rc = rc - 1;
-        }
-        if ((next_seat[r] - i) < i - l) {
-            sum = sum - (i - l) + (next_seat[r] - i);
-            if (l <= i)
-                lc--;
-            else
-                rc--;
-            if (next_seat[r] <= i)
-                rc++;
-            else
-                lc++;
-            l = next_seat[l];
-            r = next_seat[r];
-        }
-        ans = min(ans, sum);
-    }
-    cout << ans << endl;
     return 0;
 }
