cad9a632ede7d7526a56b3883def8ca2b1a1054f
kynov <2275169040@qq.com>
2020-06-05 20:01:14 +0800

    AC 1week

--- hiho/hiho308_影院座位.cpp
+++ hiho/hiho308_影院座位.cpp
@@ -15,10 +15,10 @@ int main() {
         cin >> seats[i];
         seats[i - n] = seats[i];
         seats[i + n] = seats[i];
+        seats_cnt += (seats[i] == 0);
         // if (seats[i] == 0) {
         //     seats_cnt++;
         // }
-        seats_cnt += (seats[i] == 0);
     }
     int last = 3 * n;
     for (int i = 3 * n - 1; i >= 0; i--) {
@@ -31,19 +31,37 @@ int main() {
     if (seats_cnt < m) {
         cout << -1 << endl;
     } else {
-        long long ans = LLONG_MAX;
-        for(int i=n; i<2*n; i++){
-            int l=i, r=i, s = m-(1-seats[i]);
-            long long sum = 0;
-            while (s > 0) {
-                if (s > 0) {
-                    l--;
-                    if (seats[l] == 0) s--, sum = sum + (i - l);
-                }
-                if (s > 0) {
-                    r++;
-                    if (seats[r] == 0) s--, sum = sum + (r - i);
-                }
+        int l = n, r = n, s = m - (1 - seats[n]);
+        int lc = (1 - seats[n]), rc = 0;
+        long long sum = 0, ans = LLONG_MAX;
+        // Find the nearest result for the n-th (0-th actually) seat
+        while (s > 0) {
+            if (s > 0) {
+                l--;
+                if (seats[l] == 0) s--, lc++, sum = sum + (n - l);
+            }
+            if (s > 0) {
+                r++;
+                if (seats[r] == 0) s--, rc++, sum = sum + (r - n);
+            }
+        }
+        ans = min(ans, sum);
+        while (seats[l] != 0) l++;
+        while (seats[r] != 0) r--;
+        // Translate from left to right
+        for (int i = n + 1; i <= 2 * n; i++) {
+            sum = sum + lc - rc;
+            if (seats[i] == 0) {
+                lc++;
+                rc--;
+            }
+            if (nextSeat[r] - i < i - l) {
+                sum -= (i - l);
+                sum += (nextSeat[r] - i);
+                lc--; rc++;
+                // if (l <= i) lc--; else rc--;
+                // if (nextSeat[r] <= i) lc++; else rc++;
+                l = nextSeat[l]; r = nextSeat[r];
             }
             ans = min(ans, sum);
         }
